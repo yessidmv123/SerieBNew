2c3ef4138c9ce84acf858edec3e3d0a8a9c7b26e
yessidmv123 <yessid.miranda@prodem.bo>
2026-03-04 10:04:22 -0400

    adsf

---
+++ assets/ssrBoot-wMtZC1s_.js
@@ -0,0 +1,1 @@
+import{ab as t,i as e,Y as s,bu as n}from"./index-Bl5n5Siu.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
